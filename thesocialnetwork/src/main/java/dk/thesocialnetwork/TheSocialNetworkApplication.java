9ac8c02614a2717d033432c6f75954629a915fa2
Simon Schønberg Bojesen <cph-sb339@cphbusiness.dk>
2021-05-22 12:44:25 +0200

    added empty project

---
+++ thesocialnetwork/src/main/java/dk/thesocialnetwork/TheSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package dk.thesocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheSocialNetworkApplication.class, args);
+    }
+
+}
---
+++ thesocialnetwork/src/test/java/dk/thesocialnetwork/TheSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.thesocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheSocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
