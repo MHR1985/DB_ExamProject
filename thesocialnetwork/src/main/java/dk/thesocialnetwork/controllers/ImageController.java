b70ed727c8f3d07f2534cc59bd761dc258ab0d3d
klh <klh@minuba.dk>
2021-05-28 21:44:04 +0200

    - edit ImageController to use root

--- thesocialnetwork/src/main/java/dk/thesocialnetwork/controllers/ImageController.java
+++ thesocialnetwork/src/main/java/dk/thesocialnetwork/controllers/ImageController.java
@@ -3,11 +3,12 @@ package dk.thesocialnetwork.controllers;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import javax.activation.FileTypeMap;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 
 @Controller
@@ -16,12 +17,21 @@ import java.nio.file.Files;
 public class ImageController {
 
 
-    @GetMapping("/{fileName}")
+   @GetMapping("/{fileName}")
     public ResponseEntity<byte[]> getImage(@PathVariable String fileName) throws IOException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        classLoader.getResource("resources/images/"+fileName);
-        File img = new File(classLoader.getResource("images/"+fileName).getFile());
+        File img = new File("thesocialnetwork/images/"+fileName);
         return ResponseEntity.ok().contentType(MediaType.valueOf(FileTypeMap.getDefaultFileTypeMap().getContentType(img))).body(Files.readAllBytes(img.toPath()));
+    }
+
+
+    //Use first line to save file to path, delete method after use
+    public void saveFileToRelative(String fileName) throws FileNotFoundException, UnsupportedEncodingException {
+        File test = new File("thesocialnetwork/images/"+fileName);
+        PrintWriter writer = new PrintWriter(test, "UTF-8");
+        writer.println("The first line");
+        writer.println("The second line");
+        writer.close();
+
     }
 }
 
